35b9efcf0b4e42ec17124241dcada5797bccec13
Sebastian Constantinescu <sebastian.constantinescu@selfpay.ro>
2023-04-27 12:30:49 +0300

    added RequestHandler to remove logic clutter from Controller

--- Controllers/ODataV4Controller.cs
+++ Controllers/ODataV4Controller.cs
@@ -2,6 +2,7 @@ using Flurl;
 using Microsoft.AspNetCore.Mvc;
 using ODataDBService.Models;
 using ODataDBService.Services;
+using System.Net.Mime;
 using System.Text.Json;
 
 namespace ODataDBService.Controllers
@@ -12,161 +13,141 @@ namespace ODataDBService.Controllers
     {
         private readonly ILogger<ODataV4Controller> _logger;
         private readonly IODataV4Service _oDataV4Service;
+        private readonly RequestHandler _requestHandler;
 
         public ODataV4Controller(ILogger<ODataV4Controller> logger, IODataV4Service oDataV4Service)
         {
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
             _oDataV4Service = oDataV4Service ?? throw new ArgumentNullException(nameof(oDataV4Service));
+            _requestHandler = new RequestHandler(logger);
         }
 
         [HttpGet("{tableName}", Name = "QueryRecords")]
-        public async Task<ActionResult<ODataQueryResult>> QueryAsync(string tableName,
+        public Task<IActionResult> QueryAsync(
+            string tableName,
             [FromQuery(Name = "$select")] string? select = null,
             [FromQuery(Name = "$filter")] string? filter = null,
             [FromQuery(Name = "$orderby")] string? orderby = null,
             [FromQuery(Name = "$top")] int top = 10,
             [FromQuery(Name = "$skip")] int skip = 0)
         {
-            var result = await _oDataV4Service.QueryAsync(tableName, select, filter, orderby, top, skip);
-
-            if (result == null)
-            {
-                return NotFound();
-            }
-
-            var isLastPage = result.Count <= top;
-            result.NextLink = isLastPage ? null : Url.Link("QueryRecords", new { tableName })
-                .SetQueryParam("$select", select)
-                .SetQueryParam("$filter", filter)
-                .SetQueryParam("$orderby", orderby)
-                .SetQueryParam("$top", top)
-                .SetQueryParam("$skip", skip + top);
-
-            return Ok(result);
+            return _requestHandler.HandleAsync(
+                () => _oDataV4Service.QueryAsync(tableName, select, filter, orderby, top, skip),
+                result => Ok(result),
+                "Query records");
         }
 
         [HttpDelete("{tableName}/{key}")]
-        public async Task<IActionResult> DeleteAsync(string tableName, string key)
+        public Task<IActionResult> DeleteAsync(string tableName, string key)
         {
-            var result = await _oDataV4Service.DeleteAsync(tableName, key);
-
-            if (!result)
-            {
-                return NotFound();
-            }
-
-            return Ok();
+            return _requestHandler.HandleAsync(
+                () => _oDataV4Service.DeleteAsync(tableName, key),
+                result => Ok(result),
+                "Delete record");
         }
 
         [HttpPost("{tableName}")]
-        public async Task<IActionResult> PostAsync(string tableName, [FromBody] JsonElement data)
+        public Task<IActionResult> PostAsync(string tableName, [FromBody] JsonElement data)
         {
-            var result = await _oDataV4Service.InsertAsync(tableName, data);
-
-            if (!result)
-            {
-                return BadRequest();
-            }
-
-            return Ok();
+            return _requestHandler.HandleAsync(
+                () => _oDataV4Service.InsertAsync(tableName, data),
+                result => Ok(result),
+                "Insert record");
         }
 
         [HttpPut("{tableName}({key})")]
-        public async Task<IActionResult> PutAsync(string tableName, string key, [FromBody] JsonElement data)
+        public Task<IActionResult> PutAsync(string tableName, string key, [FromBody] JsonElement data)
         {
-            var result = await _oDataV4Service.UpdateAsync(tableName, key, data);
-
-            if (!result)
-            {
-                return BadRequest();
-            }
-
-            return Ok();
+            return _requestHandler.HandleAsync(
+                () => _oDataV4Service.UpdateAsync(tableName, key, data),
+                result => Ok(result),
+                "Update record");
         }
-
-        // Note: The ExecuteStoredProcedure method should also be refactored to use the service and repository pattern.
-        // ...
-        //[HttpPost("{storedProcedureName}")]
-        //public IActionResult ExecuteStoredProcedure(string storedProcedureName, [FromBody] JsonElement procedureParameters)
-        //{
-        //    // Validate the input
-        //    if (procedureParameters.ValueKind != JsonValueKind.Object)
-        //    {
-        //        return BadRequest("Procedure parameters must be a JSON object.");
-        //    }
-
-        //    // Get the expected parameter names and types from the stored procedure
-        //    var expectedParameters = GetStoredProcedureParameters(storedProcedureName);
-
-        //    // Validate that each expected parameter is present in the input
-        //    foreach (var expectedParameter in expectedParameters)
-        //    {
-        //        if (!procedureParameters.TryGetProperty(expectedParameter.Name, out var parameterValue))
-        //        {
-        //            return BadRequest($"Procedure parameter '{expectedParameter.Name}' is missing.");
-        //        }
-
-        //        // Map the JSON value kind to its corresponding SQL data type
-        //        string parameterType = "";
-        //        switch (parameterValue.ValueKind)
-        //        {
-        //            case JsonValueKind.String:
-        //                parameterType = "nvarchar";
-        //                break;
-        //            case JsonValueKind.Number:
-        //                parameterType = "decimal";
-        //                break;
-        //            case JsonValueKind.True:
-        //            case JsonValueKind.False:
-        //                parameterType = "bit";
-        //                break;
-        //            case JsonValueKind.Null:
-        //                parameterType = "nvarchar";
-        //                break;
-        //            default:
-        //                return BadRequest($"Procedure parameter '{expectedParameter.Name}' has an invalid type. Expected '{expectedParameter.Type}', but got '{parameterValue.ValueKind}'.");
-        //        }
-
-        //        // Validate the parameter value's type against the expected type
-        //        if (parameterType != expectedParameter.Type)
-        //        {
-        //            return BadRequest($"Procedure parameter '{expectedParameter.Name}' has an invalid type. Expected '{expectedParameter.Type}', but got '{parameterType}'.");
-        //        }
-        //    }
-
-        //    try
-        //    {
-        //        // Execute the stored procedure
-        //        using (var connection = new SqlConnection(_connectionString))
-        //        {
-        //            connection.Open();
-        //            var result = connection.Query<dynamic>(storedProcedureName, (object)procedureParameters, commandType: System.Data.CommandType.StoredProcedure);
-        //            return Ok(result);
-        //        }
-        //    }
-        //    catch (SqlException ex)
-        //    {
-        //        return StatusCode(500, ex.Message);
-        //    }
-        //}
-
-        //private IEnumerable<StoredProcedureParameter> GetStoredProcedureParameters(string storedProcedureName)
-        //{
-        //    // Get the expected parameter names and types from the stored procedure
-        //    using (var connection = new SqlConnection(_connectionString))
-        //    {
-        //        connection.Open();
-        //        var command = new SqlCommand($"SELECT PARAMETER_NAME, DATA_TYPE FROM INFORMATION_SCHEMA.PARAMETERS WHERE SPECIFIC_NAME = '{storedProcedureName}'", connection);
-        //        var reader = command.ExecuteReader();
-        //        while (reader.Read())
-        //        {
-        //            yield return new StoredProcedureParameter
-        //            {
-        //                Name = (string)reader["PARAMETER_NAME"],
-        //                Type = (string)reader["DATA_TYPE"]
-        //            };
-        //        }
-        //    }
-        //}
     }
+
+    // Note: The ExecuteStoredProcedure method should also be refactored to use the service and repository pattern.
+    // ...
+    //[HttpPost("{storedProcedureName}")]
+    //public IActionResult ExecuteStoredProcedure(string storedProcedureName, [FromBody] JsonElement procedureParameters)
+    //{
+    //    // Validate the input
+    //    if (procedureParameters.ValueKind != JsonValueKind.Object)
+    //    {
+    //        return BadRequest("Procedure parameters must be a JSON object.");
+    //    }
+
+    //    // Get the expected parameter names and types from the stored procedure
+    //    var expectedParameters = GetStoredProcedureParameters(storedProcedureName);
+
+    //    // Validate that each expected parameter is present in the input
+    //    foreach (var expectedParameter in expectedParameters)
+    //    {
+    //        if (!procedureParameters.TryGetProperty(expectedParameter.Name, out var parameterValue))
+    //        {
+    //            return BadRequest($"Procedure parameter '{expectedParameter.Name}' is missing.");
+    //        }
+
+    //        // Map the JSON value kind to its corresponding SQL data type
+    //        string parameterType = "";
+    //        switch (parameterValue.ValueKind)
+    //        {
+    //            case JsonValueKind.String:
+    //                parameterType = "nvarchar";
+    //                break;
+    //            case JsonValueKind.Number:
+    //                parameterType = "decimal";
+    //                break;
+    //            case JsonValueKind.True:
+    //            case JsonValueKind.False:
+    //                parameterType = "bit";
+    //                break;
+    //            case JsonValueKind.Null:
+    //                parameterType = "nvarchar";
+    //                break;
+    //            default:
+    //                return BadRequest($"Procedure parameter '{expectedParameter.Name}' has an invalid type. Expected '{expectedParameter.Type}', but got '{parameterValue.ValueKind}'.");
+    //        }
+
+    //        // Validate the parameter value's type against the expected type
+    //        if (parameterType != expectedParameter.Type)
+    //        {
+    //            return BadRequest($"Procedure parameter '{expectedParameter.Name}' has an invalid type. Expected '{expectedParameter.Type}', but got '{parameterType}'.");
+    //        }
+    //    }
+
+    //    try
+    //    {
+    //        // Execute the stored procedure
+    //        using (var connection = new SqlConnection(_connectionString))
+    //        {
+    //            connection.Open();
+    //            var result = connection.Query<dynamic>(storedProcedureName, (object)procedureParameters, commandType: System.Data.CommandType.StoredProcedure);
+    //            return Ok(result);
+    //        }
+    //    }
+    //    catch (SqlException ex)
+    //    {
+    //        return StatusCode(500, ex.Message);
+    //    }
+    //}
+
+    //private IEnumerable<StoredProcedureParameter> GetStoredProcedureParameters(string storedProcedureName)
+    //{
+    //    // Get the expected parameter names and types from the stored procedure
+    //    using (var connection = new SqlConnection(_connectionString))
+    //    {
+    //        connection.Open();
+    //        var command = new SqlCommand($"SELECT PARAMETER_NAME, DATA_TYPE FROM INFORMATION_SCHEMA.PARAMETERS WHERE SPECIFIC_NAME = '{storedProcedureName}'", connection);
+    //        var reader = command.ExecuteReader();
+    //        while (reader.Read())
+    //        {
+    //            yield return new StoredProcedureParameter
+    //            {
+    //                Name = (string)reader["PARAMETER_NAME"],
+    //                Type = (string)reader["DATA_TYPE"]
+    //            };
+    //        }
+    //    }
+    //}
+}
 }
--- Repositories/ODataV4Repository.cs
+++ Repositories/ODataV4Repository.cs
@@ -4,6 +4,7 @@ using ODataDBService.Models;
 using System.Data.SqlClient;
 using System.Text.Json;
 using ODataDBService.Extensions;
+using System.Text;
 
 namespace ODataDBService.Repositories
 {
@@ -47,10 +48,14 @@ namespace ODataDBService.Repositories
 
         public async Task<bool> DeleteAsync(string tableName, string key)
         {
-            var (query, queryParams) = _oDataToSqlConverter.ConvertToSQLDelete(tableName, key, _connectionString);
+            var tableInfo = GetTableInfo(tableName);
+
+            var sql = $"DELETE FROM {tableInfo.TableName} WHERE {tableInfo.PrimaryKey} = @{tableInfo.PrimaryKey}";
+            var parameters = new DynamicParameters();
+            parameters.Add(tableInfo.PrimaryKey, key);
 
             await using var conn = new SqlConnection(_connectionString);
-            var result = await conn.ExecuteAsync(query, queryParams).ConfigureAwait(false);
+            var result = await conn.ExecuteAsync(sql, parameters).ConfigureAwait(false);
 
             return result > 0;
         }
@@ -60,10 +65,19 @@ namespace ODataDBService.Repositories
             var properties = data.EnumerateObject()
                 .ToDictionary(prop => prop.Name, prop => prop.Value);
 
-            var (query, queryParams) = _oDataToSqlConverter.ConvertToSQLInsert(tableName, properties);
+            var columnNames = string.Join(", ", properties.Keys);
+            var valueParams = string.Join(", ", properties.Keys.Select(key => $"@{key}"));
+
+            var sql = $"INSERT INTO {tableName} ({columnNames}) VALUES ({valueParams})";
+            var parameters = new DynamicParameters();
+
+            foreach (var (key, value) in properties)
+            {
+                parameters.Add(key, value.ToObject());
+            }
 
             await using var conn = new SqlConnection(_connectionString);
-            var result = await conn.ExecuteAsync(query, queryParams).ConfigureAwait(false);
+            var result = await conn.ExecuteAsync(sql, parameters).ConfigureAwait(false);
 
             return result > 0;
         }
@@ -71,13 +85,67 @@ namespace ODataDBService.Repositories
         public async Task<bool> UpdateAsync(string tableName, string key, JsonElement data)
         {
             var properties = data.EnumerateObject().ToDictionary(prop => prop.Name, prop => prop.Value);
+            var tableInfo = GetTableInfo(tableName);
 
-            var (query, queryParams) = _oDataToSqlConverter.ConvertToSQLUpdate(tableName, key, properties, _connectionString);
+            var queryParams = new DynamicParameters();
+
+            var sb = new StringBuilder($"UPDATE {tableInfo.TableName} SET ");
+            foreach (var property in properties)
+            {
+                var propertyName = property.Key;
+                var propertyValue = property.Value;
+
+                if (propertyName.Equals(tableInfo.PrimaryKey, StringComparison.OrdinalIgnoreCase))
+                {
+                    continue;
+                }
+
+                if (tableInfo.ColumnNames?.Contains(propertyName, StringComparer.OrdinalIgnoreCase) != true)
+                {
+                    throw new ArgumentException($"Column {propertyName} does not exist in table {tableName}");
+                }
+
+                sb.Append($"{propertyName} = @{propertyName}, ");
+                queryParams.Add(propertyName, propertyValue.ToObject());
+            }
+
+            sb.Remove(sb.Length - 2, 2); // remove last comma and space
+            sb.Append($" WHERE {tableInfo.PrimaryKey} = @{tableInfo.PrimaryKey}");
+            queryParams.Add(tableInfo.PrimaryKey, key);
 
             await using var conn = new SqlConnection(_connectionString);
-            var result = await conn.ExecuteAsync(query, queryParams).ConfigureAwait(false);
+            var result = await conn.ExecuteAsync(sb.ToString(), queryParams).ConfigureAwait(false);
 
             return result > 0;
+        }
+
+        private TableInfo GetTableInfo(string tableName)
+        {
+            using var connection = new SqlConnection(_connectionString);
+
+            var tableInfo = new TableInfo
+            {
+                TableName = tableName,
+                PrimaryKey = connection.QueryFirstOrDefault<string>(
+                    @"SELECT COLUMN_NAME
+                    FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE
+                    WHERE TABLE_NAME = @TableName 
+                    AND CONSTRAINT_NAME LIKE 'PK%'",
+                    new { TableName = tableName })
+            };
+
+            if (tableInfo.PrimaryKey == null)
+            {
+                throw new ArgumentException($"Could not find primary key for table {tableName}");
+            }
+
+            tableInfo.ColumnNames = connection.Query<string>(
+                @"SELECT COLUMN_NAME
+                FROM INFORMATION_SCHEMA.COLUMNS
+                WHERE TABLE_NAME = @TableName",
+                new { TableName = tableName });
+
+            return tableInfo;
         }
     }
 }
