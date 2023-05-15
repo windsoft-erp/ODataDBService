594d3cac4fc205e2bfd28d988d9152b7b2c04c9d
constantinescu-sebastian <130563417+constantinescu-sebastian@users.noreply.github.com>
2023-05-15 13:58:05 +0300

    added args to prog

--- ODataDBUp/Program.cs
+++ ODataDBUp/Program.cs
@@ -5,87 +5,128 @@ using System.Reflection;
 using Dapper;
 
 string connectionString = GetConnectionString();
-if (connectionString==null)
+if (connectionString == null)
 {
     Console.WriteLine("Connection string not found in configuration.");
     return;
 }
 
-while (true)
+if (args.Length > 0)
 {
-    Console.Clear();
-    Console.ForegroundColor=ConsoleColor.Cyan;
-    Console.WriteLine("=== Database Management ===");
-    Console.ResetColor();
-    Console.WriteLine("1. Deploy scripts to existing database.");
-    Console.WriteLine("2. Drop the database and deploy scripts.");
-    Console.WriteLine("3. Exit");
-    Console.WriteLine();
-    Console.Write("Enter the option number: ");
-
-    if (!int.TryParse(Console.ReadLine(), out int option))
-    {
-        Console.ForegroundColor=ConsoleColor.Yellow;
-        Console.WriteLine("Invalid input. Press any key to try again.");
-        Console.ResetColor();
-        Console.ReadKey();
-        continue;
-    }
-
-    if (option==3)
-    {
-        break;
-    }
-
-    try
+    if (int.TryParse(args[0], out int option))
     {
         switch (option)
         {
             case 1:
-                CreateDatabaseIfNotExists(connectionString);
-                DeployScripts(connectionString);
-                Console.ForegroundColor=ConsoleColor.Green;
-                Console.WriteLine("Operation completed successfully. Press any key to continue.");
-                Console.ResetColor();
-                Console.ReadKey();
+                ExecuteOperation1();
                 break;
             case 2:
-                Console.ForegroundColor=ConsoleColor.Red;
-                Console.WriteLine("WARNING: This will permanently delete the database and all data it contains.");
-                Console.ResetColor();
-                Console.Write("Are you sure you want to continue? Enter 'yes' to confirm, or any other key to cancel: ");
-                var confirm = Console.ReadLine();
-
-                if (confirm.ToLower()=="yes")
-                {
-                    DropDatabase(connectionString);
-                    CreateDatabaseIfNotExists(connectionString);
-                    DeployScripts(connectionString);
-                    Console.ForegroundColor=ConsoleColor.Green;
-                    Console.WriteLine("Operation completed successfully. Press any key to continue.");
-                    Console.ResetColor();
-                    Console.ReadKey();
-                }
-                else
-                {
-                    Console.WriteLine("Operation cancelled. Press any key to continue.");
-                    Console.ReadKey();
-                }
+                ExecuteOperation2("yes");
                 break;
             default:
-                Console.ForegroundColor=ConsoleColor.Yellow;
-                Console.WriteLine("Invalid input. Press any key to try again.");
-                Console.ResetColor();
-                Console.ReadKey();
+                Console.WriteLine("Invalid argument. Available options: 1, 2");
                 break;
         }
     }
-    catch (Exception ex)
+    else
     {
-        Console.ForegroundColor=ConsoleColor.Red;
-        Console.WriteLine($"An error occurred: {ex.Message}");
+        Console.WriteLine("Invalid argument. Please provide a valid option: 1 or 2");
+    }
+}
+else
+{
+    RunStandardMode();
+}
+
+void RunStandardMode()
+{
+    while (true)
+    {
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine("=== Database Management ===");
         Console.ResetColor();
-        Console.WriteLine("Press any key to continue.");
+        Console.WriteLine("1. Deploy scripts to existing database.");
+        Console.WriteLine("2. Drop the database and deploy scripts.");
+        Console.WriteLine("3. Exit");
+        Console.WriteLine();
+        Console.Write("Enter the option number: ");
+
+        if (!int.TryParse(Console.ReadLine(), out int option))
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Invalid input. Press any key to try again.");
+            Console.ResetColor();
+            Console.ReadKey();
+            continue;
+        }
+
+        if (option == 3)
+        {
+            break;
+        }
+
+        try
+        {
+            switch (option)
+            {
+                case 1:
+                    ExecuteOperation1();
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Operation completed successfully. Press any key to continue.");
+                    Console.ResetColor();
+                    Console.ReadKey();
+                    break;
+                case 2:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    ExecuteOperation2();
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Operation completed successfully. Press any key to continue.");
+                    Console.ResetColor();
+                    Console.ReadKey();
+                    break;
+                default:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Invalid input. Press any key to try again.");
+                    Console.ResetColor();
+                    Console.ReadKey();
+                    break;
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"An error occurred: {ex.Message}");
+            Console.ResetColor();
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+        }
+    }
+}
+
+void ExecuteOperation1()
+{
+    CreateDatabaseIfNotExists(connectionString);
+    DeployScripts(connectionString);
+}
+
+void ExecuteOperation2(string? confirmed = null)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("WARNING: This will permanently delete the database and all data it contains.");
+    Console.ResetColor();
+    Console.Write("Are you sure you want to continue? Enter 'yes' to confirm: ");
+    var confirm = confirmed ?? Console.ReadLine();
+
+    if (confirm.ToLower() == "yes")
+    {
+        DropDatabase(connectionString);
+        CreateDatabaseIfNotExists(connectionString);
+        DeployScripts(connectionString);
+    }
+    else
+    {
+        Console.WriteLine("Operation cancelled. Press any key to continue.");
         Console.ReadKey();
     }
 }
